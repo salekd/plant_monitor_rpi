6a4def990cf267d252c3eca768f788c1244d699f
David Salek <salekd@gmail.com>
2018-04-13 10:59:45 +0200

    use uid instead of MAC address for photos

--- take_photo.py
+++ take_photo.py
@@ -10,12 +10,12 @@ import os
 # Read config file
 config = ConfigParser()
 config.read('raspberrypi.cfg')
-mac = config.get('MiFlora', 'mac')
+uid = config.get('RaspberryPi', 'uid')
 url = config.get('Flask', 'url')
 
 # Define an image filename with the device id and a timestamp
 filename = "/home/pi/plant_monitor_rpi/images/{}_{}.jpg".format(\
-        mac.replace(':', ''), datetime.utcnow().strftime("%Y%m%d%H%M%S"))
+        uid, datetime.utcnow().strftime("%Y%m%d%H%M%S"))
 
 # Take photo
 camera = picamera.PiCamera()
