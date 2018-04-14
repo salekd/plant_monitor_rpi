2cd164e539bb1308d674c2af18b40f3c0c1c7ce4
David Salek <salekd@gmail.com>
2018-04-14 22:13:43 +0200

    logging

--- pump.py
+++ pump.py
@@ -3,6 +3,24 @@ import RPi.GPIO as GPIO
 from miflora.miflora_poller import MiFloraPoller
 from miflora.backends.bluepy import BluepyBackend
 from configparser import ConfigParser
+import logging
+import sys
+
+
+# Create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create handler
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setLevel(logging.INFO)
+
+# Specify the logging format
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(handler)
 
 
 # Read config file
@@ -17,10 +35,12 @@ pump_gpio = config.getint('Pump', 'pump_gpio')
 poller = MiFloraPoller(mac, BluepyBackend)
 poller.fill_cache()
 measurement = poller._parse_data()
-print(measurement)
+logger.info("moisture = {}, temperature = {}, conductivity = {}, light = {}".format(
+    measurement["moisture"], measurement["temperature"],
+    measurement["conductivity"], measurement["light"]))
 
 if measurement['moisture'] < moisture_threshold:
-    print("Pumping water for {} s.".format(pump_time))
+    logger.info("Pumping water for {} s.".format(pump_time))
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pump_gpio, GPIO.OUT, initial=GPIO.HIGH)
     sleep(pump_time)
