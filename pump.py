74ed4dd50b3411a3d9ffb3a884a1f3289a6bd1c4
David Salek <salekd@gmail.com>
2018-04-24 01:08:19 +0200

    upload pump watering log

--- pump.py
+++ pump.py
@@ -5,6 +5,10 @@ from miflora.backends.bluepy import BluepyBackend
 from configparser import ConfigParser
 import logging
 import sys
+from datetime import datetime
+import time
+import json
+import requests
 
 
 # Create logger
@@ -27,6 +31,7 @@ logger.addHandler(handler)
 config = ConfigParser()
 config.read('raspberrypi.cfg')
 mac = config.get('MiFlora', 'mac')
+url = config.get('Flask', 'url')
 moisture_threshold = config.getint('Pump', 'moisture_threshold')
 pump_time = config.getint('Pump', 'pump_time')
 pump_gpio = config.getint('Pump', 'pump_gpio')
@@ -47,3 +52,18 @@ if measurement['moisture'] < moisture_threshold:
     sleep(pump_time)
     GPIO.output(pump_gpio, GPIO.LOW)
     GPIO.cleanup()
+
+    pump_log = {}
+    pump_log["timestamp"] = str(datetime.utcnow())
+    pump_log["device"] = mac
+    pump_log["duration"] = pump_time
+
+    # Append to a csv file for the device
+    csvfile = "/home/pi/plant_monitor_rpi/measurements/pump_{}.csv".format(mac.replace(':', ''))
+    with open(csvfile, "a") as f:
+        # Make sure the entries are in the correct order
+        f.write("{}, {}\n".format(pump_log["timestamp"], pump_log["duration"]))
+
+    # Upload to server
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    requests.post(url + "/pump", data=json.dumps(pump_log), headers=headers)
